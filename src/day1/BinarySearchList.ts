9b309db9a6578d00cc4290a5fad621ff09dcc705
Jai Kumar <jaikumar1240@gmail.com>
2024-08-17 22:38:40 +0530

    Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length-1;
+    while(lo<=hi){
+    let mid = Math.floor(lo+(hi-lo)/2);
+        if(haystack[mid]==needle) return true;
+        else if(haystack[mid]>needle){
+            hi = mid-1;
+        }
+        else{
+            lo = mid+1;
+        }
+    }
+    return false;
 }
