8a9219efd44c1ab8862beaf483e7f145482f8f98
Jai Kumar <jaikumar1240@gmail.com>
2024-08-18 10:40:40 +0530

    two crystal ball using binary

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    let lo = 0;
+    let hi = breaks.length-1;
+    let mid = Math.floor(lo+(hi-lo)/2);
+    let first_break = -1;
+    while(lo<=hi){
+        if(breaks[mid]){
+            first_break = mid;
+            hi = mid-1;
+        }
+        else{
+            lo = mid+1;
+        }
+        mid = Math.floor(lo+(hi-lo)/2);
+    }
+    return first_break;
 }
