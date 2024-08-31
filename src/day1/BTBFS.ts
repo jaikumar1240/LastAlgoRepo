79bd4de5cb9865fd801629a7657199a1f81c0bc5
Jai Kumar <jaikumar1240@gmail.com>
2024-08-31 21:44:01 +0530

    Binary tree Breadth search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,18 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: BinaryNode<number>[] = [head];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        if (!current) continue;
+        if (current.value === needle) {
+            return true;
+        }
+        if (current.left) {
+            queue.push(current.left);
+        }
+        if (current.right) {
+            queue.push(current.right)
+        }
+    }
+    return false;
 
 }
